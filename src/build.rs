4a1f765e0f4dfeba6189d00ea795cc591d963859
seu <seu@panopticon.re>
2016-06-21 01:10:58 +0200

    fix build on osx, 2nd try

--- src/build.rs
+++ src/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     // for brew
     if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-search=native=/usr/local");
+        println!("cargo:rustc-link-search=native=/usr/local/lib");
     }
 
     println!("cargo:rustc-flags=-l glpk");
