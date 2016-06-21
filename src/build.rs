43a2f67d62b0d40b23c0df147c19099e16e1366f
seu <seu@panopticon.re>
2016-06-21 13:13:57 +0200

    fix build on osx, 11th try

--- src/build.rs
+++ src/build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     // for brew
-    if cfg!(target_os = "macos") {
+    /*if cfg!(target_os = "macos") {
         let maybe_brew_prfx = Command::new("brew")
             .args(&vec!["--prefix","homebrew/science/glpk"])
             .output().ok().and_then(|x| { String::from_utf8(x.stdout).ok() });
@@ -11,7 +11,7 @@ fn main() {
             Some(x) => println!("cargo:rustc-link-search=native={}/lib",x),
             None => {},
         }
-    }
+    }*/
 
     println!("cargo:rustc-flags=-l glpk");
 }
