67bf8063fe258d6ae3030520b8677e5558865936
seu <seu@panopticon.re>
2016-06-23 14:51:54 +0200

    fix lib path

--- src/build.rs
+++ src/build.rs
@@ -1,14 +1,17 @@
 use std::process::Command;
+use std::path::PathBuf;
 
 fn main() {
     // for brew
     if cfg!(target_os = "macos") {
         let maybe_brew_prfx = Command::new("brew")
             .args(&vec!["--prefix","homebrew/science/glpk"])
-            .output().ok().and_then(|x| { String::from_utf8(x.stdout).ok() });
+            .output().ok().and_then(|x| { String::from_utf8(x.stdout).ok() })
+            .and_then(|x| { x.lines().next().map(|x| x.to_string()) })
+            .map(|x| { PathBuf::from(&x) });
 
         match maybe_brew_prfx {
-            Some(x) => println!("cargo:rustc-link-search=native={}/lib",x),
+            Some(x) => println!("cargo:rustc-link-search=native={}",x.join("lib").display()),
             None => {},
         }
     }
